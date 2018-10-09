d704a564d7b17fd68f03173d5b1bd68d217e3f67
Syed Hassaan Ahmed <syah@microsoft.com>
2018-10-09 14:05:30 +0200

    Add RG creation in ARM template

--- setup.sh
+++ setup.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-RESOURCE_GROUP=syah-data-manager
-STORAGE_ACCOUNT_PREFIX=datamanager
-DATABRICKS_CLUSTER_NAME=datamanager-cluster
+# Define parameters with default values
+RESOURCE_GROUP=${RESOURCE_GROUP:=syah-data-manager}
+STORAGE_ACCOUNT_PREFIX=${STORAGE_ACCOUNT_PREFIX:=datamanager}
+SQL_ADMIN_PASSWORD=${SQL_ADMIN_PASSWORD:='<YourStrong!Passw0rd>'}
+DATABRICKS_CLUSTER_NAME=${DATABRICKS_CLUSTER_NAME:=datamanager-cluster}
 
+# Create resource group if needed
+RG_LOCATION=westeurope
+RG_EXISTS=$(az group exists -n $RESOURCE_GROUP -o tsv)
+if [[ "$RG_EXISTS" != true ]]; then
+  az group create -l $RG_LOCATION -n $RESOURCE_GROUP
+fi
+
+# Deploy ARM Template
+#az group deployment create -g $RESOURCE_GROUP --template-file azuredeploy.json --parameters sqlAdminPassword=$SQL_ADMIN_PASSWORD
+
+# Configure Databricks Token
+DATABRICKS_CONFIG=~/.databrickscfg
+if [[ ! -f "$DATABRICKS_CONFIG" ]]; then
+  databricks configure --token
+fi
+DATABRICKS_TOKEN=$(grep token $DATABRICKS_CONFIG | awk -F ' = ' '{print $2}')
+
+# Create Databricks Cluster if needed
 CLUSTER_EXISTS=$(databricks clusters list | tr -s " " | cut -d" " -f2 | grep ^${DATABRICKS_CLUSTER_NAME}$)
 if [[ -n $CLUSTER_EXISTS ]]; then
     echo "Cluster $DATABRICKS_CLUSTER_NAME already exists."
@@ -31,12 +51,18 @@ EOF
 fi
 
 CLUSTER_ID=$(databricks clusters list | awk '/'$DATABRICKS_CLUSTER_NAME'/ {print $1}')
+
+# Create Storage Container
 STORAGE_ACCOUNT_NAME=$(az storage account list -g $RESOURCE_GROUP --query "[?starts_with(name, '$STORAGE_ACCOUNT_PREFIX')].name" -o tsv)
 STORAGE_ACCOUNT_KEY=$(az storage account keys list -g $RESOURCE_GROUP -n $STORAGE_ACCOUNT_NAME --query "[0].value" -o tsv)
 STORAGE_ACCOUNT_CONTAINER=data-manager
 
 az storage container create --account-name $STORAGE_ACCOUNT_NAME --account-key $STORAGE_ACCOUNT_KEY --name $STORAGE_ACCOUNT_CONTAINER
 
+# Import all Databricks Notebooks
+databricks workspace import_dir -o notebooks /Shared
+
+# Mount Storage Container in DBFS
 BODY_CREATE_JOB_RUN=$(cat << EOF
 {
     "run_name": "Mount Azure Storage",
